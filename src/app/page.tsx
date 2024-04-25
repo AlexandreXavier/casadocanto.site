843889bd80db61a44d51a3e45fe84a7c5d23ef89
AlexandreXavier <jalexandre.xavier@gmail.com>
2024-04-25 09:21:52 +0100

    correct some issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,24 @@ import { db } from "~/server/db";
 
 export const dynamic ="force-dynamic"
 
-const moskUrl = [
-  "https://utfs.io/f/46c12ca2-9739-4821-9e65-36648a2f59f6-dbjphc.jpg",
-  "https://utfs.io/f/bcb7e426-d357-4b52-a206-7617850eb35a-35kigo.jpeg",
-  "https://utfs.io/f/c8016421-fc2f-4bf1-b272-ad11406b5495-ge19g9.jpeg",
-  "https://utfs.io/f/ada024b3-a843-483b-8294-9f0862983389-ecb4b8.jpeg",
-  "https://utfs.io/f/7e45db59-d779-40ee-8af5-7e65762af383-2e512i.jpeg",
-];
-
-const mockImages = moskUrl.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 
 export default async function HomePage() {
 
-  const posts= await db.query.posts.findMany();
-  console.log(posts)
-
+  const images= await db.query.images.findMany({
+    orderBy:(model,{desc})=> desc(model.id),
+  });
+  
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post)=>(
-        <div key={post.id}>{post.name}</div>
-      ))}
-        {[...mockImages,...mockImages, ...mockImages, ...mockImages].map((image,index) => (
-          <div key={image.id+"-"+index} className="w-48">
+       
+        {[...images,...images, ...images, ...images].map((image,index) => (
+          <div key={image.id+"-"+index} className="w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
+
       </div>
     </main>
   );
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,11 +18,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `casadocanto.site_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "image",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
