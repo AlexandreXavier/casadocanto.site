09e7e5c07ee7af90d332454ce4265e15f340ef7d
AlexandreXavier <jalexandre.xavier@gmail.com>
2024-05-01 10:40:25 +0100

    fix Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar"
 
 const inter = Inter({
   subsets: ["latin"],
